81b7325e78dac5bae59a843adfaeffc9457431ab
LYJie <2417208288@qq.com>
2019-12-21 23:39:10 +0800

    test

--- zx_zpp/lib/pages/login_page.dart
+++ zx_zpp/lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'dart:io';
 
 
 
@@ -18,20 +20,34 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
-    print("height:${ScreenUtil.screenHeight}");
-    print("height:${ScreenUtil.screenWidth}");
+    ScreenUtil.instance = ScreenUtil(width: 1080, height: 2358)..init(context);
     return Center(
        child: Text("test"),
     );
   }
 
 
-  // Widget get cover =>AnimatedPositioned(
-  //   duration: const Duration(seconds:2),
-  //   top: 0.0,
-  //   curve: Curves.easeIn,
-  //   width: ,
-  // );
+  Widget get cover => AnimatedPositioned(
+        duration: const Duration(seconds: 2),
+        top: -ScreenUtil.screenHeight ,
+        curve: Curves.easeIn,
+        width: ScreenUtil.screenWidth,
+        height: -ScreenUtil.screenHeight,
+        child: Container(
+          padding: EdgeInsets.all(ScreenUtil().setHeight(80)),
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+          ),
+          child: Center(
+            child: Image.asset(
+              "123.png",
+              width: ScreenUtil().setHeight(240),
+            ),
+          ),
+        ),
+      );
+
+
+  
 
 }
